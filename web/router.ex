813947ebddbd67a816a30db28b46b874e5f486f7
Quan Vu <nongdenchet8435848@gmail.com>
2019-09-19 10:15:58 +0800

    Remove html supports

--- web/router.ex
+++ web/router.ex
@@ -1,21 +1,9 @@
 defmodule Kurrency.Router do
   use Kurrency.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
     plug Kurrency.TokenPlug
-  end
-
-  scope "/", Kurrency do
-    pipe_through :browser
   end
 
   scope "/api", Kurrency do
